92870949394de1c8b91acd872f8e7db7d2c14205
shovampo <shivammoudgil358@gmail.com>
2022-12-15 18:13:49 +0530

    fd

---
+++ A4store/src/pages/api/order/[id].js
@@ -0,0 +1,40 @@
+import dbConnect from "../../../../utils/mongo";
+import Order from "../../../../models/Order";
+
+const handler = async (req, res) => {
+  const {
+    method,
+    query: {id},
+  } = req;
+
+  await dbConnect();
+
+  if (method === "GET") {
+    try {
+      const order = await Order.findById(id);
+      res.status(200).json(order);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+  if (method === "PUT") {
+    try {
+      const order = await Order.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      res.status(200).json(order);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+    if (method === "DELETE") {
+         try {
+           const order = await Order.findByIdAndDelete(id)
+           res.status(200).json("Deleted successfully");
+         } catch (err) {
+           res.status(500).json(err);
+         }
+  }
+};
+
+export default handler;
---
+++ A4store/src/pages/order/checkout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const checkout = () => {
+  return (
+    <div>
+      checkout
+    </div>
+  )
+}
+
+export default checkout
