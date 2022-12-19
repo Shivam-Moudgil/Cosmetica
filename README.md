0c9f780aedc9df6e0685edf169cde33fc906a355
Shrikant <shrikantjawla@gmail.com>
2022-12-19 11:37:38 +0530

    final change

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Avatarstore
-Its an ecommerce website where you can buy all type of groceries.
+//React-icons , Chakra UI is already installed !
+
+//Tarun - Homepage Navbar Footer;
+
+//Zuhed - Page Strucutre;
+
+//Onkar - Authentication;
+
+//Shrikanth- Admin;
+
+//Shivam - Data / Cart ;
+
+//Data -
+USer-
+Name - email - Password - timestamps;
+Product -
+qty
+Cart -
+Qty , userID , Prod. ID
+Purchase -
+UserID , Prod.ID , OrderDate , DeliveryDate , qty , TypeofPayment
+//
+# Project Code:  full-volleyball-922
+.env.local = mongodb+srv://shivam:moudgil@cluster0.7ilvy1y.mongodb.net/Avatar?retryWrites=true&w=majority
--- src/pages/api/cart/index.js
+++ src/pages/api/cart/index.js
@@ -1,10 +1,9 @@
 import dbConnect from "../../../../utils/mongo";
 import CartItems from "../../../../models/Cart";
 import Products from "../../../../models/Product";
-import jwt from "jsonwebtoken";
-import {verifyUser} from "../../../../middlewares/authMiddleware";
+import { verifyUser } from "../../../../middlewares/authMiddleware";
 export default async function handler(req, res) {
-  const {method, cookies} = req;
+  const { method, cookies } = req;
   dbConnect();
 
   // const token = cookies.OursiteJWT;
@@ -27,7 +26,7 @@ export default async function handler(req, res) {
 
   if (method === "POST") {
     verifyUser(req, res);
-    const {product, quantity} = req.body;
+    const { product, quantity } = req.body;
     try {
       //checking existing quantity
       let productInWarehouse = await Products.findById(product);
@@ -37,15 +36,15 @@ export default async function handler(req, res) {
         );
       }
 
-      let existing = await CartItems.findOne({product, user: req.user.id});
+      let existing = await CartItems.findOne({ product, user: req.user.id });
       if (existing) {
         await CartItems.updateOne(
-          {product, user: req.user.id},
-          {$set: {quantity: existing.quantity + quantity}},
-          {new: true}
+          { product, user: req.user.id },
+          { $set: { quantity: existing.quantity + quantity } },
+          { new: true }
         );
       } else {
-        await CartItems.create({user: req.user.id, product, quantity});
+        await CartItems.create({ user: req.user.id, product, quantity });
       }
       // console.log(product);
       res.status(201).send("item has been updated in cart.");
