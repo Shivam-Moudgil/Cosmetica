74f1ce6f4485be3b65324c29c9fc32611ad17879
Shivam Moudgil <84116631+Shivam-Moudgil@users.noreply.github.com>
2022-12-21 20:05:12 +0530

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,15 +1,14 @@
 import jwt from "jsonwebtoken";
 
-export const verifyUser = (handler) => {
-  return async (req, res) => {
+export const verifyUser = (req, res) => {
     const token = req.cookies.OursiteJWT;
-    if (!token) return res.status(401).json("You are not Authenticated");
+  if (!token) return false
 
-    jwt.verify(token, process.env.Secret_key, (err, user) => {
-      if (err) return res.status(404).json("Invalid Token!");
+  jwt.verify(token, process.env.Secret_key, (err, user) => {
+    if (err) return false
 
-      req.user = user;
-      return handler(req, res);
+    req.user = user;
+    return { req, res }
     });
   };
-};
+
